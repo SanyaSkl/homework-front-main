16775be66c29ab8447efdc0a69c4e063c0b58450
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-06-08 16:22:56 +0300

    homework 3-4

--- src/HW/4/Button.tsx
+++ src/HW/4/Button.tsx
@@ -14,29 +14,3 @@ export const Button = (props: ButtonPropsType) => {
     </button>
   );
 };
-
-
-
-
-
-
-
-
-
-
-//type ButtonPropsType = {
-//   callBack: () => void
-//   name: string
-// };
-//
-// export const Button = (props: ButtonPropsType) => {
-//   const callBackHandler = () => {
-//     props.callBack()
-//   };
-//
-//   return (
-//     <button id={'hw04-button'} onClick={callBackHandler}>
-//       {props.name}
-//     </button>
-//   );
-// };
