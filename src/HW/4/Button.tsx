902772dcd9e78aaa53d5f335c25c0a36e2ef7db0
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-06-08 16:22:16 +0300

    homework 3-4

--- src/HW/4/Button.tsx
+++ src/HW/4/Button.tsx
@@ -1,11 +1,11 @@
 type ButtonPropsType = {
-  callBack: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-  name: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+  callBack: () => void
+  name: string
 };
 
 export const Button = (props: ButtonPropsType) => {
   const callBackHandler = () => {
-    // НУЖНО ДОПИСАТЬ
+    props.callBack()
   };
 
   return (
@@ -14,3 +14,29 @@ export const Button = (props: ButtonPropsType) => {
     </button>
   );
 };
+
+
+
+
+
+
+
+
+
+
+//type ButtonPropsType = {
+//   callBack: () => void
+//   name: string
+// };
+//
+// export const Button = (props: ButtonPropsType) => {
+//   const callBackHandler = () => {
+//     props.callBack()
+//   };
+//
+//   return (
+//     <button id={'hw04-button'} onClick={callBackHandler}>
+//       {props.name}
+//     </button>
+//   );
+// };
